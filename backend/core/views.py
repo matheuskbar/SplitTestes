7fb0e847f29be4dac96b06ccb9d59ef0686b3b3f
DESKTOP-N7HHFNK\Matheus <matheuskbar@gmail.com>
2021-11-29 20:12:07 -0200

    primeiros testes

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from reportlab.lib.pagesizes import A4
-from reportlab.pdfgen import canvas
-from reportlab.lib.units import inch
-from reportlab.lib.colors import pink, black, red, blue, green
+# from epub.reader import content
 
 
 def index(request):
+    # mybook = content.open_epub('meuEbook.epub')
+    #
+    # for item in mybook.opf.manifest.values():
+    # # read the content
+    # data = mybook.read_item(item)
+    #
+    # print(dir(mybook))
     return render(request, 'index.html')
